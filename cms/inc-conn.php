fa883bfd59e4172999dda54594efe55572f16bc5
3leanore <nymanchristine@gmail.com>
2016-07-26 13:20:16 +0200

    Live host details added

--- cms/inc-conn.php
+++ cms/inc-conn.php
@@ -1,8 +1,17 @@
 <?php
-	$vconnServer = 'localhost';
-	$vconnDbUsername = 'dev';
-	$vconnDbPassword = 'Ramfest2014';
-	$vconnDatabase = 'dbcreativeangels';
+// LOCALHOST
+/*$vconnServer = 'localhost';
+$vconnUsername = 'dev';
+$vconnPassword = 'Ramfest2014';
+$vconnDatabase = 'dbcreativeangels';*/
+
+// LIVE HOST
+$vconnServer = 'sql30.jnb2.host-h.net';
+$vconnUsername = 'creative01x';
+$vconnPassword = 'nNAaJN2Bbg8';
+$vconnDatabase = 'creativeangelsdb';
+// RW F5UYyMQq948
+// RO LW1AF9XxCk8
 
 	//Connect to MYSQL server
 	$vconn_creativeangels = mysqli_connect($vconnServer, $vconnDbUsername, $vconnDbPassword, $vconnDatabase);
@@ -19,4 +28,5 @@
 		mysqli_select_db($vconn_creativeangels, $vDatabase);
 
 	}
+
 ?>
--- inc-conn.php
+++ inc-conn.php
@@ -1,8 +1,17 @@
 <?php
-$vconnServer = 'localhost';
+// LOCALHOST
+/*$vconnServer = 'localhost';
 $vconnUsername = 'dev';
 $vconnPassword = 'Ramfest2014';
-$vconnDatabase = 'dbcreativeangels';
+$vconnDatabase = 'dbcreativeangels';*/
+
+// LIVE HOST
+$vconnServer = 'sql30.jnb2.host-h.net';
+$vconnUsername = 'creative01x';
+$vconnPassword = 'nNAaJN2Bbg8';
+$vconnDatabase = 'creativeangelsdb';
+// RW F5UYyMQq948
+// RO LW1AF9XxCk8
 
 //Connect to MYSQL server
 $vconn_creativeangels = mysqli_connect($vconnServer, $vconnUsername, $vconnPassword, $vconnDatabase);
