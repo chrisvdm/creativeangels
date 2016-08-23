69d69c471d08f9bb0a39936a174625b33794fee3
3leanore <nymanchristine@gmail.com>
2016-08-23 10:50:35 +0200

    Add image upload to root

---
+++ file-upload-resize/image-upload-form.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<form action="image-upload-process.php" method="post" enctype="multipart/form-data">
+
+	<input type="file" name="txtimage">
+    
+    <input type="submit" value="Upload image">
+
+</form>
+
+</body>
+</html>
