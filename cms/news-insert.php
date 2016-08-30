5b573ed08859c311b6b1a43e624aaf9021a8ef8b
3leanore <nymanchristine@gmail.com>
2016-08-30 12:17:05 +0200

    Create news component files

---
+++ cms/news-insert.php
@@ -0,0 +1,48 @@
+<?php require('inc-cms-pre-doctype.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Head contents -->
+    <?php require('inc-cms-head-content.php'); ?>
+
+  </head>
+  <body>
+    <!-- PAGE WRAPPER -->
+    <div id="page-wrapper">
+
+      <!-- SIDEBAR MAIN MENU -->
+      <?php require('inc-cms-sidebar.php'); ?>
+
+        <!-- RIGHT COLUMN MAIN CONTENT CONTAINER -->
+      <section class="right-content-wrapper">
+
+        <!-- HEADER -->
+        <header class="base">
+
+          <!-- Branding container -->
+          <?php require('inc-cms-branding-container.php'); ?>
+
+          <!-- Page title -->
+          <div class="page-header">
+            <h2>Insert news article</h2>
+          </div>
+
+        </header>
+
+        <!-- MAIN CONTENT SECTION -->
+        <section id="main-content" class="base">
+
+          <form>
+            
+          </form>
+
+        </section>
+
+      </section>
+      <div class="clearfix"></div>
+
+    </div>
+
+    <script src="js/accordian.js"></script>
+  </body>
+</html>
